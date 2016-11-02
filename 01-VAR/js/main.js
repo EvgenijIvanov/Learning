d54a3010fede8ed32286ea9d9c79d5149b168528
Евгений Иванов <evgeney.ivanov@gmail.com>
2016-11-02 20:21:49 +0200

    Мой первый "комит" через консоль. Хо-хо-хо!!!

---
+++ 01-VAR/js/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var message = null;
+var her     = undefined;
+console.log(her);
+var rezultat =(her==message);
+console.log(rezultat);
+
+var team = {
+    TeamLead: 'Егор',
+    Admin: 'Петро',
+    Selector: 'Василий',
+    Provaider: 'Инокентий'
+}
+console.log(team);
+console.log(typeof team);
+
+var a = (5,6);
+console.log(a);
