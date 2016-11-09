90d6a526d812fe45764213d96d55e7abef959777
Евгений Иванов <evgeney.ivanov@gmail.com>
2016-11-09 19:14:14 +0200

    Исправленное ДЗ по масивам, условно второе (после 4 урока)

--- DZ/DZ2/main.js
+++ DZ/DZ2/main.js
@@ -12,25 +12,28 @@ var mass = new Array();
 var i;
 for(i=0; i<30; i++){
     if (i%2 > 0){
-        mass.push(rand(-50,-90))
-    } else {
         mass.push(rand(50,90))
+    } else {
+        mass.push(rand(-50,-90))
     }
 }
+mass.unshift(undefined);
+mass.pop();
 console.log(mass);
-for(i=0; i<mass.length; i=i+5){
+
+for(i=4; i<mass.length; i=i+5){
     console.log(mass[i]);
 }
 alert ('Вариант второй');
 var mass3 = new Array();
 var j;
-for(i=0; i<30; i=i+2){
+for(i=2; i<30; i=i+2){
     mass3[i] = rand(100,10);
     for(j=1; j<30; j=j+2){
         mass3[j] = rand(-100,-10)
     }
 }
 console.log(mass3);
-for(i=0; i<mass3.length; i=i+5){
+for(i=4; i<mass3.length; i=i+5){
     console.log(mass3[i]);
 }
