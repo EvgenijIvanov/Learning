761ed645017787180253558a3a0c6e4a04c5f220
Евгений Иванов <evgeney.ivanov@gmail.com>
2016-12-07 23:21:43 +0200

    Морской Бой версия 0.02

--- SeaBattle/test.js
+++ SeaBattle/test.js
@@ -7,32 +7,40 @@ var seabatle = {
 			for (var i = 0; i < n; i++){
 			this.battleField[i] = new Array(n)
 					for(var j = 0; j < n; j++){
-					this.battleField[i][j] = {ship: false,
-																		shoot: false,
-																		missed: false,
-																		kill: false,
-																		wound: false,
-																		margin: false}
+					this.battleField[i][j] = { ship: false,
+														shoot: false,
+														missed: false,
+														kill: false,
+														wound: false,
+														margin: false}
 					}
 			}
 	},
-	putShip: function(x,y){
-		(this.validationSize(x,y)) ? this.battleField[x][y].ship = true : console.log('Неверные координаты!');
-		// for(var i = (x-1); i <= (x +1); i++ ){
-		// 	for(var j = (j-1); j <= (x+1); j++){
-		// 		this.battleField[i][j].margin = true;
-		// 	}
-		// }
+	putShip: function(x,y,n){
+		(this.validationSize(x,y) && this.validationMargin(x,y)) ? this.putMargin(x,y,n) : console.log('Неверные данные!');
+	},
+	putMargin: function(x,y,n){
+		debugger;
+		if (n <= 0) return (return n);
+			for(var i = (x-1); i <= (x + 1); i++ ){
+					if(i < 0 || i > this.battleField.length - 1) continue;
+						for(var j = (y-1); j <= (y+1); j++){
+							if(j < 0 || j > this.battleField.length - 1) continue;
+									this.battleField[i][j].margin = true;
+									this.battleField[x][y].ship = true;
+								}
+						seabatle.putMargin((x+1),y,(n - 1))
+				}
 	},
 	validationSize: function(x,y){
 		return (x > this.battleField.length -1 || y > this.battleField.length - 1) ? false : true //&& alert('Наше поле '+ this.battleField.length + 'на' + this.battleField.length + '! Введите координаты в нужных пределах');
-	}
-	validationShip: function(x,y){
-		return (this.battleField[x][y].ship == true) ? false: true
+	},
+	validationMargin: function(x,y){
+		return (this.battleField[x][y].margin == true) ? false: true
 	}
 }	
-seabatle.creatingField(3);
-seabatle.putShip(4,1);
+seabatle.creatingField(4);
+seabatle.putShip(1,1,2);
 console.table(seabatle.battleField);
 
 // var a = new Array(5);
