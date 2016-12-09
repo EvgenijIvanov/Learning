a37e588cd98f19f9cf98a97bc7eb22971a761bd4
Евгений Иванов <evgeney.ivanov@gmail.com>
2016-12-10 00:46:26 +0200

    Морской бой версия 0.03 Работают методы по созданию поля битвы, создания и размещания кораблей с учетом палубности, размера поля, и растояния между ними.

--- SeaBattle/test.js
+++ SeaBattle/test.js
@@ -2,6 +2,8 @@
 
 var seabatle = {
 	battleField: null,
+	shipLenght: null,
+	messageError:"0 error!",
 	creatingField: function(n){
 		this.battleField = new Array(n);
 			for (var i = 0; i < n; i++){
@@ -14,41 +16,80 @@ var seabatle = {
 														wound: false,
 														margin: false}
 					}
-			}
+			}																		// shipLenght - палубность корабля от 1 до 4
+	},																				// x,y - начальные координаты для строительства корабля, 
+	requestBuildingShip : function(x,y,shipLenght,direction){   // direction - вектор направления: принимает значения '1' - вертикальный корабль, или "0" - горизонтальнный						
+		// debugger;
+		if (this.validationSize(x,y) && this.validationMargin(x,y) && this.validationshipLenght(x,y,shipLenght,direction)) {	
+		 	this.buildingShip(x,y,direction);
+		} else {
+			console.log(this.messageError);
+		}
 	},
-	putShip: function(x,y,n){
-		(this.validationSize(x,y) && this.validationMargin(x,y)) ? this.putMargin(x,y,n) : console.log('Неверные данные!');
-	},
-	putMargin: function(x,y,n){
-		debugger;
-		if (n <= 0) return (return n);
-			for(var i = (x-1); i <= (x + 1); i++ ){
+	buildingShip: function(x,y,direction){
+			// debugger;
+			for(var i = (x-1); i <= (x + 1); i++ ){					// циклы для "построения" вокруг корабля "рамки" толщиной 1 квадрат
 					if(i < 0 || i > this.battleField.length - 1) continue;
 						for(var j = (y-1); j <= (y+1); j++){
 							if(j < 0 || j > this.battleField.length - 1) continue;
 									this.battleField[i][j].margin = true;
-									this.battleField[x][y].ship = true;
 								}
-						seabatle.putMargin((x+1),y,(n - 1))
+				}
+				this.shipLenght --;
+				this.battleField[x][y].ship = true;
+				if (this.shipLenght <= 0) {
+					return console.log('Корабль готов!');
+				} else if(direction == 1){
+					this.buildingShip((x+1),y,direction);
+				} else {
+					this.buildingShip(x,(y+1),direction);
 				}
 	},
 	validationSize: function(x,y){
-		return (x > this.battleField.length -1 || y > this.battleField.length - 1) ? false : true //&& alert('Наше поле '+ this.battleField.length + 'на' + this.battleField.length + '! Введите координаты в нужных пределах');
+		// debugger;
+		if (x > this.battleField.length || y > this.battleField.length) {
+			this.messageError = 'Координаты за пределами поля боя!';
+			return false;
+		} else {
+			return true;
+		}
 	},
 	validationMargin: function(x,y){
-		return (this.battleField[x][y].margin == true) ? false: true
+		// debugger;
+		if (this.battleField[x][y].margin == true) {
+			this.messageError = 'Слишком близко к другому кораблю! Задайте другие координаты!';
+			return false;
+		} else {
+			return true;
+		}
+	},
+	validationshipLenght: function(x,y,shipLenght,direction){
+			// debugger;
+		if(shipLenght >= 1 && shipLenght <= 4){
+			if(direction == 1){
+				if((x+shipLenght) <= this.battleField.length) {
+					this.shipLenght = shipLenght;
+					return true;
+				} else {
+					this.messageError = 'Ваш корабль вылазит за край мира! Он упадет! Задайте другие координаты! (ось Х)';
+					return false;
+				}
+			} else if (direction == 0){
+				if((y+shipLenght) <= this.battleField.length){
+					this.shipLenght = shipLenght;
+					return true;
+				}else {
+					this.messageError = 'Ваш корабль вылазит за край мира! Он упадет! Задайте другие координаты! (ось Y)';
+					return false;
+						}
+			} 
+		} else {
+			this.messageError = 'Задайте другую палубность! Не более 4 палуб!';
+			return false;
+		}
 	}
 }	
-seabatle.creatingField(4);
-seabatle.putShip(1,1,2);
+seabatle.creatingField(10);
+seabatle.requestBuildingShip(3,3,4,1);
 console.table(seabatle.battleField);
 
-// var a = new Array(5);
-// for (var i = 0; i < 5; i++){
-// 		a[i] = new Array(5)
-// 		for(var j = 0; j < 5; j++){
-// 			a[i][j] = {ok: true}
-// 		}
-// }
-
-// console.table(a);
